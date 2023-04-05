f76628f98d47686e7cc716734a722571aa00be37
h3nrey <pedro.novaes.education@gmail.com>
2023-04-04 23:55:24 -0300

    basic ball movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from sys import exit;
 
 W = 800;
@@ -16,18 +17,42 @@ class Ball(pygame.sprite.Sprite):
         self.image = pygame.Surface((self.w, self.w));
         self.image.fill("white");
         self.rect = self.image.get_rect(center = (W / 2, H/2));
+        self.senseX = 1;
+        self.senseY = -1;
+        self.speed = 4;
+    
+    def move(self):
+        x = self.rect.x;
+        y = self.rect.y;
+        speed = self.speed;
+        sense = self.senseX;
+
+        if(x >= 775):
+            self.senseX = -1;
+        elif(x < 0):
+            self.senseX = 1;
+        if(y > 475 or y < 0):
+            self.senseY *= -1;
+        self.rect.x += speed * sense;
+        self.rect.y += speed * self.senseY;
+    
+    def update(self):
+        self.move();
 
 ball = pygame.sprite.GroupSingle();
 ball.add(Ball());
 
 while True:
     events = pygame.event.get();
+    screen.fill("black");
 
     for event in events:
         if(event.type == pygame.QUIT):
             pygame.quit();
             exit();
     
+    ball.update();
     ball.draw(screen);
+
     pygame.display.update();
     clock.tick(FPS);
