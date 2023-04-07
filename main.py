aa8777f8622e514a35034b5c789312889e36b228
h3nrey <pedro.novaes.education@gmail.com>
2023-04-07 10:06:55 -0300

    handle end game and display who won

--- main.py
+++ main.py
@@ -10,13 +10,22 @@ screen.fill("black");
 
 clock = pygame.time.Clock();
 FPS = 60;
-
+matchFinished = False;
+whoWon = "";
 def RestartRally(madePoint):
+    # if(madePoint == 1):
+    #     paddle.sprites()[1].score += 1;
+    # else: paddle.sprites()[0].score += 1;
     paddle.sprites()[madePoint].score += 1;
+    print(f"pos: {paddle.sprites()[madePoint].rect.x} - score: {paddle.sprites()[madePoint].score}")
     
     for p in paddle.sprites():
         p.rect.y = (500 / 2) - 50;
 
+def OnWin():
+    global matchFinished;
+    matchFinished = True;
+    print(f"{whoWon} won")
 class Ball(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__();
@@ -53,9 +62,9 @@ class Ball(pygame.sprite.Sprite):
         
         if(x >= 775 or x < 0):
             if(x >= 775):
-                madePoint = 0
+                madePoint = 1
             elif(x < 0):
-                madePoint = 1;
+                madePoint = 0;
             self.RestartPos();
             callback(madePoint);
     
@@ -81,6 +90,7 @@ class Paddle(pygame.sprite.Sprite):
         self.speed = 4;
         self.score = 0;
         self.type = type;
+        # print(f"pos: {self.rect.x} - type: {self.type}")
 
     def move(self, ballPos):
         if(self.rect.y > 400): self.rect.y = H - self.rect.height;
@@ -90,11 +100,12 @@ class Paddle(pygame.sprite.Sprite):
             self.rect.y += self.speed * self.sense;
         else:
             ballSense = 0;
-            if(ballPos > H / 2): ballSense = 1;
-            elif(ballPos < H/2): ballSense = -1;
-            print(ballPos);
-            self.rect.y = ballPos * 0.5
-            # self.rect.y += self.speed * ballSense
+            if(ballPos == self.rect.y):
+                ballSense = 0;
+            else:
+                if(self.rect.y > ballPos): ballSense = 1;
+                elif(self.rect.y < ballPos): ballSense = -1;
+            self.rect.y += self.speed * ballSense
 
     def CheckInput(self, events):
         for event in events:
@@ -105,9 +116,17 @@ class Paddle(pygame.sprite.Sprite):
                     self.sense = -1;
             if(event.type == pygame.KEYUP):
                 self.sense = 0;
-    def update(self, events, ballPos):
+   
+    def CheckScore(self, callback):
+        global whoWon;
+        # print(f"score: {self.score} - type: {self.type}");
+        if(self.score >= 2):
+            whoWon = self.type;
+            callback();
+    def update(self, events, ballPos, callback):
         self.CheckInput(events);
         self.move(ballPos);
+        self.CheckScore(callback)
 
 class ScoreText(pygame.sprite.Sprite):
     def __init__(self, x):
@@ -119,16 +138,20 @@ class ScoreText(pygame.sprite.Sprite):
     def update(self, score):
         self.text = str(score);
         self.image = self.font.render(str(score), False, "#ffffff");
+
 ball = pygame.sprite.GroupSingle();
 ball.add(Ball());
 
 paddle = pygame.sprite.Group();
-paddle.add(Paddle(750, "ai"));
+paddle.add(Paddle(750, "bot"));
 paddle.add(Paddle(50, "player"));
 
 scoreText = pygame.sprite.Group();
 scoreText.add(ScoreText(200));
 scoreText.add(ScoreText(600));
+
+text = pygame.font.Font("Assets/pixel.ttf", 90);
+Restarttext = pygame.font.Font("Assets/pixel.ttf", 40);
 
 while True:
     events = pygame.event.get();
@@ -138,15 +161,34 @@ while True:
         if(event.type == pygame.QUIT):
             pygame.quit();
             exit();
+        if(event.type == pygame.KEYDOWN):
+            if(event.key == pygame.K_r):
+                matchFinished = False;
+                whoWon = "";
+                for p in paddle.sprites():
+                    p.rect.y = (500 / 2) - 50;
+                    p.score = 0;
     scoreText.draw(screen);
-    scoreText.sprites()[0].update(paddle.sprites()[0].score);
-    scoreText.sprites()[1].update(paddle.sprites()[1].score);
+    scoreText.sprites()[0].update(paddle.sprites()[1].score);
+    scoreText.sprites()[1].update(paddle.sprites()[0].score);
 
-    ball.update(paddle, RestartRally);
-    ball.draw(screen);
+    if(matchFinished == False):
+        ball.update(paddle, RestartRally);
+        ball.draw(screen);
 
-    paddle.update(events, ball.sprites()[0].rect.y);
+    if(matchFinished == False):
+        paddle.update(events, ball.sprites()[0].rect.y, OnWin);
     paddle.draw(screen);
+    
+    if(matchFinished == True):     
+        textSurf = text.render(f"{whoWon} won", False, "white");
+        textRect = textSurf.get_rect(center = (W / 2, H / 2))
+        pygame.draw.rect(screen, "black", (W / 2 - 100, H / 2 - 50, 100, 50));
+        screen.blit(textSurf, textRect);
+
+        RestartSurf = Restarttext.render(f"press R to restart", False, "white");
+        restartRect = textSurf.get_rect(center = (380, 350))
+        screen.blit(RestartSurf, restartRect);
 
     pygame.display.update();
     clock.tick(FPS);
